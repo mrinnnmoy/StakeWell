df12efb3a7907683f5465666cc0489310515b231
mrinnnmoy <mrinmoy202000@gmail.com>
2023-08-29 09:56:17 +0530

    initial-commit

---
+++ components/StakeToken.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function StakeToken() {
+  return (
+    <div>StakeToken</div>
+  )
+};
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Container, Heading } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+
+  return (
+    <Container>
+      <Heading>StakeWell</Heading>
+    </Container>
+  );
+};
+
+export default Home;
