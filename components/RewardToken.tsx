3fc5b14ef514692e35c0596aeab9c4cc113298aa
mrinnnmoy <mrinmoy202000@gmail.com>
2023-08-29 19:22:50 +0530

    build-RewardToken

--- components/RewardToken.tsx
+++ components/RewardToken.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Card, Heading, Skeleton, Stack, Text } from "@chakra-ui/react";
+import { useAddress, useContract, useTokenBalance } from "@thirdweb-dev/react";
+import { REWARD_TOKEN_ADDRESSES } from "../constants/addresses";
 
 export default function RewardToken() {
+  const address = useAddress();
+
+  const { contract: stakeTokenContract, isLoading: loadingStakeToken } =
+    useContract(REWARD_TOKEN_ADDRESSES);
+
+  const { data: tokenBalance, isLoading: loadingTokenBalance } =
+    useTokenBalance(stakeTokenContract, address);
+
   return (
-    <div>RewardToken</div>
-  )
-};
+    <Card p={5}>
+      <Stack>
+        <Heading>Reward Token</Heading>
+        <Skeleton
+          h={8}
+          w={"50%"}
+          isLoaded={!loadingStakeToken && !loadingTokenBalance}
+        >
+          <Text fontSize={"large"} fontWeight={"bold"} h={8}>
+            ${tokenBalance?.displayValue} {tokenBalance?.symbol} 
+          </Text>
+        </Skeleton>
+      </Stack>
+    </Card>
+  );
+}
